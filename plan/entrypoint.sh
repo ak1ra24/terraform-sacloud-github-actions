3afc74a0940ecb535be406d5fecada7beac37bc2
ak1ra24 <ak1ra24net@gmail.com>
2020-04-29 16:41:24 +0900

    del RUN install.sh

--- plan/entrypoint.sh
+++ plan/entrypoint.sh
@@ -24,6 +24,16 @@ fi
 
 set -e
 
+if [ -z "$TF_SACLOUD_PROVIDER_VERSION" ]; then
+	TF_SACLOUD_PROVIDER_VERSION=$(curl -s https://api.github.com/repos/sacloud/terraform-provider-sakuracloud/releases | jq -r '.[0] | .tag_name' | sed -e 's/[^0-9\.]//g')
+fi
+
+echo "$TF_SACLOUD_PROVIDER_VERSION download..."
+wget https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v${TF_SACLOUD_PROVIDER_VERSION}/terraform-provider-sakuracloud_${TF_SACLOUD_PROVIDER_VERSION}_linux-amd64.zip
+unzip terraform-provider-sakuracloud_${TF_SACLOUD_PROVIDER_VERSION}_linux-amd64.zip
+rm terraform-provider-sakuracloud_${TF_SACLOUD_PROVIDER_VERSION}_linux-amd64.zip
+mv terraform-provider-sakuracloud* terraform-provider-sakuracloud
+
 cd "${TF_ACTION_WORKING_DIR:-.}"
 
 if [[ ! -z "$TF_ACTION_TFE_TOKEN" ]]; then
